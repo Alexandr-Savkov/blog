4f6aa5305a7fe21a9beb942f422f0cabe3cd1375
alexandr <savkov.00@mail.ru>
2017-10-02 18:22:40 +0300

    day#1 angularRoute

---
+++ appModule.js
@@ -0,0 +1,26 @@
+var app = angular.module('blog', ['ngRoute']);
+
+app.config(['$routeProvider', stateConfig]);
+
+function stateConfig ($routeProvider) {
+  $routeProvider
+    .when('/list', {
+      controller: 'articlesListCtrl',
+      templateUrl: 'articles-list/articles-list.html'
+    })
+    .when('/one', {
+      controller: 'articleCtrl',
+      templateUrl: 'article/article.html'
+    })
+    .when('/profile', {
+      controller: 'setProfileCtrl',
+      templateUrl: 'set-profile/set-profile.html'
+    })
+    .when('/add', {
+      controller: 'addArticleCtrl',
+      templateUrl: 'add-article/add-article.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+};
